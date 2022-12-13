b0ee4b2ce1d47c8dc4cbfb2a6dac6f43f2296da1
frcarrascom <66007921+frcarrascom@users.noreply.github.com>
2022-12-13 21:03:41 +0100

    Delete ProyectoFinalFranciscoCarrasco directory

--- ProyectoFinalFranciscoCarrasco/js/sweetAlertIndex.js
+++
@@ -1,7 +0,0 @@
-swal.fire({
-    title: "Una Subasta ha finalizado",
-    text: "mira tus mensajes para mas informacion!",
-    type: "warning",
-    confirmButtonColor: '#3085d6',
-    confirmButtonText: 'ok!',
-});
--- ProyectoFinalFranciscoCarrasco/js/sweetAlertMensajeNoVisto.js
+++
@@ -1,7 +0,0 @@
-swal.fire({
-    title: "Tienes mensajes nuevos sin leer",
-    text: "mira tus mensajes para mas informacion!",
-    type: "warning",
-    confirmButtonColor: '#3085d6',
-    confirmButtonText: 'ok!',
-});
